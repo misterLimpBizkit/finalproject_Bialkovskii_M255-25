b8a7529ea9bbcefc6826d81575c52dee3e39a463
MisterSigmaMac <egorzapekanka@gmail.com>
2025-12-01 22:22:15 +0300

    main.py was fixed

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 
 from valutetrade_hub.cli.interface import main
-import scheduler
-from valutetrade_hub.parser_service.updater import RatesUpdater
 
 if __name__ == "__main__":
-    # Start the currency rates scheduler
-    result = RatesUpdater()
-    result.update_rates()
-    
-    # Start the CLI interface
     main()
